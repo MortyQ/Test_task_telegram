7cb2af183e842808ae8ed1de7f8c16da0e6597fa
MortyQ <yasenev1998@gmail.com>
2020-12-02 14:43:13 +0200

    Finish

--- src/Main_page_component.jsx
+++ src/Main_page_component.jsx
@@ -6,8 +6,6 @@ import { UserOutlined } from '@ant-design/icons';
 
 const MainTelegramm =()=>{
 
-
-
     const [state, setState] =useState({
         data: '',
         time: '',
@@ -15,24 +13,49 @@ const MainTelegramm =()=>{
     });
 
 
+
+    const { TelegramClient } = require('messaging-api-telegram');
+ 
+
+const client = new TelegramClient({
+  accessToken: '1437432634:AAEfa3yM3DmDOOh5LTMzbY0Z37jDGXMQFo4',
+});
+
+client.getWebhookInfo().catch((error) => {
+    console.log(error); // formatted error message
+    console.log(error.stack); // error stack trace
+    console.log(error.config); // axios request config
+    console.log(error.request); // HTTP request
+    console.log(error.response); // HTTP response
+  });
+
+
     const handle =(event)=>{
         setState({ ...state, [event.target.name]: event.target.value})
     }
 
+    
 
-
+    // await client.sendMessage('TestTaskTelegrammMessagebot', 'hi', {
+    //     disableWebPagePreview: true,
+    //     disableNotification: true,
+    //   });
     const push_info =()=>{
-
-        
+        console.log(state.data,state.time,state.adress)
+        const message = 'Дата: ' + state.data +',  '+
+            'Время: ' + state.time + ',  '+
+            'Адресс: '+ state.adress
+        const token = '1437432634:AAEfa3yM3DmDOOh5LTMzbY0Z37jDGXMQFo4'
+        let url = 'https://api.telegram.org/bot'+token+'/sendMessage?chat_id=-348038280&text='
+        const xhttp = new XMLHttpRequest();
+        xhttp.open('GET', url+message, true);
+        xhttp.send();
     }
 
-            return(
-                
-
+    return(            
                 <div className="main_telegram">
-
+                        <h1>Тестовое задание для собеседования</h1>
                         <div className="position_section">
-
     <Input  onChange={handle} name='data' size="large" placeholder="data:" prefix={<UserOutlined />} />
     {state.data}
     <br />
